ac401cb9647f45ec8d27d2c2200d9ed47c1fae6e
theapache64 <theapache64@gmail.com>
2021-02-28 12:54:31 +0530

    🌟 add dialog issue

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,28 +1,59 @@
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
 fun main() = Window {
-    var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-
-            // Do network call
-            doNetworkCall()
-
-
-        }) {
-            Text(text)
+        Column {
+            NetworkButton()
+            Spacer(modifier = Modifier.height(10.dp))
+            DialogButton()
         }
+    }
+}
+
+@Composable
+fun DialogButton() {
+    var shouldShowDialog by remember { mutableStateOf(false) }
+    Button(
+        onClick = {
+            shouldShowDialog = true
+        }
+    ) {
+        Text(text = "SHOW DIALOG")
+    }
+
+    if (shouldShowDialog) {
+
+        Dialog(
+            onDismissRequest = {
+                shouldShowDialog = false
+            }
+        ) {
+            Text(text = "Sample content")
+        }
+    }
+}
+
+@Composable
+fun NetworkButton() {
+    Button(onClick = {
+        // Do network call
+        doNetworkCall()
+
+    }) {
+        Text(text = "Make Network Call")
     }
 }
 
